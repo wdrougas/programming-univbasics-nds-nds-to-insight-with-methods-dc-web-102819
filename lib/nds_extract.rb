ff00f03ec7e94d506ee758cff42584d6acf16edf
Will Drougas <wdrougas@gmail.com>
2019-10-16 19:07:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,9 @@ require 'directors_database'
 
 def directors_totals(nds)
   result = {}
-  nil
+  i = 0
+  while i < nds.length do
+    
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
