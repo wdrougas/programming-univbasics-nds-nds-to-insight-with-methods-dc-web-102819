b539baf2b9cd5a0b0fbc8d70064a7647d61256db
Will Drougas <wdrougas@gmail.com>
2019-10-16 20:05:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,15 +11,12 @@ def directors_totals(nds)
   i = 0
   while i < nds.length do
     director = nds[i]
-    # We want to provide the director name and their gross income, each on a new line
     result[director[:name]] = gross_for_director(director_data)
     i += 1
   end
   result
 end
 
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# using director_data as input
 def gross_for_director(director_data)
   i = 0
   total = 0
